5210de878f65172c84bb0b66cb2569b9b6ae21a7
Wayne Dovey <wdovey@gmail.com>
2023-03-23 22:00:32 +1100

    update tests

--- seed-metallb.sh
+++ seed-metallb.sh
@@ -4,7 +4,8 @@ oc get nodes 2> /dev/null
 
 if [ $? -eq 0 ]
 then
-  oc create namespace metallb-system
+  #oc create namespace metallb-system
+  oc new-project metallb-system
   oc project metallb-system
   oc create -f operator/operator-group.yaml
   oc create -f operator/mlb-operator-subscription.yaml
@@ -22,13 +23,22 @@ done
 
 # Create the CRD
 oc create -f operator/custom-resource.yaml
-until oc get mch -n metallb-system | grep Running 2> /dev/null
+until oc get MetalLB -n metallb-system | grep metall 2> /dev/null
 do
     echo echo "Wait for CRD Installation"
     sleep 30
 done
 
+# Show worker nodes IP addresses
+
+oc get node $(oc get nodes | grep worker | grep -v master | awk '{print $1}')  -o wide | awk -v OFS='\t\t' '{print $6}' | grep -v INTERNAL-IP
+
 # Applied all MLB bits
+
+firewall-cmd --add-port=179/tcp
+firewall-cmd --add-port=3784/udp
+firewall-cmd --add-port=3785/udp
+firewall-cmd --runtime-to-permanent
 
 oc apply -f mlb/address-pool-bgp.yaml
 oc apply -f mlb/test-bfd-prof.yaml
@@ -37,5 +47,12 @@ oc apply -f mlb/peer-test.yaml
 # Start BGP Emulator Router
 
 podman run -d --rm  -v /root/metallb-lab/frr:/etc/frr:Z --net=host --name frr-upstream --privileged quay.io/frrouting/frr:master
+
+# Create a test SVC
+
+oc new-project test-metallb
+oc create deployment hello-node --image=k8s.gcr.io/e2e-test-images/agnhost:2.33 -- /agnhost serve-hostname
+
+oc create -f mlb/test-frr-svc.yaml
 
 
